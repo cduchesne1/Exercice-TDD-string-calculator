37235f9efbf733c4a01e75dc465d9931100e7337
Christophe Duchesne <cduchesne.1@gmail.com>
2022-03-26 17:24:30 -0400

    setp 1

--- src/main/java/com/github/glo2003/Calculator.java
+++ src/main/java/com/github/glo2003/Calculator.java
@@ -1,11 +1,21 @@
 package com.github.glo2003;
 
 public class Calculator {
-    public int add(String numbers) {
-        if (numbers.isEmpty()) {
-            return 0;
-        }
-
-        throw new InvalidNumberFormatException();
+  public int add(String numbers) {
+    if (numbers.isEmpty()) {
+      return 0;
     }
+
+    String[] split = numbers.split(",");
+    int sum = 0;
+
+    try {
+      for (String number : split) {
+        sum += Integer.parseInt(number);
+      }
+    } catch (NumberFormatException e) {
+      throw new InvalidNumberFormatException();
+    }
+    return sum;
+  }
 }
--- src/test/java/com/github/glo2003/CalculatorTest.java
+++ src/test/java/com/github/glo2003/CalculatorTest.java
@@ -1,10 +1,10 @@
 package com.github.glo2003;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
@@ -25,6 +25,20 @@ public class CalculatorTest {
     @Test
     void whenNonNumericValues_thenThrowInvalidInput() {
         assertThrows(InvalidNumberFormatException.class,
-                () -> calculator.add("4,a"));
+            () -> calculator.add("4,a"));
+    }
+
+    @Test
+    void whenSingleNumber_thenReturnsNumber() {
+        int result = calculator.add("1");
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    void whenTwoNumbers_thenReturnsSum() {
+        int result = calculator.add("1,2");
+
+        assertEquals(3, result);
     }
 }
